952b33a1a337a17113c3ec613d0d1c0a6a269836
Iago <iagocabral.software@gmail.com>
2024-05-07 22:33:52 -0300

    basic sonar test

---
+++ sonar_test.py
@@ -0,0 +1,14 @@
+#Neste código, há alguns problemas que o SonarCloud pode apontar
+def calcular_fatorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * calcular_fatorial(n-1)
+
+def main():
+    num = int(input("Digite um número inteiro positivo para calcular o fatorial: "))
+    resultado = calcular_fatorial(num)
+    print(f"O fatorial de {num} é {resultado}")
+
+if __name__ == "__main__":
+    main()
