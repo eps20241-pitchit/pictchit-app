4975551f2b488a782a73757f9d95851cc1301a80
Pedro Nogueira <88006041+phnog@users.noreply.github.com>
2024-06-06 15:56:47 -0300

    Add files via upload

---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './home.css';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+    <div className="home-container">
+      <h1>Quem somos</h1>
+      <p>
+        Somos um grupo de estudantes da Universidade de Brasília (UNB) dedicados a desenvolver soluções inovadoras no campo da tecnologia. Nosso projeto de Engenharia de Produto de Software (EPS) resume-se na criação de um meio que aproxime as empresas dos projetos desenvolvidos pelos estudantes.
+      </p>
+      <p>
+        Nossa equipe é composta por estudantes de engenharia de software da Faculdade do Gama (FGA). Estamos unidos em nosso compromisso de simplificar a criação de pitch dos inúmeros projetos criados no nosso campus.
+      </p>
+      <p>
+        Nosso objetivo é de contribuir com um sistema que aproxime investidores das tecnologias desenvolvidas por estudantes e que podem render bons frutos e negócios. Estamos ansiosos para enfrentar os desafios do projeto e fazer uma diferença positiva na vida das pessoas.
+      </p>
+      <div className="unb-logo">
+        <img src="https://asmetro.org.br/portalsn/wp-content/uploads/2016/11/UnB.png" alt="UnB Logo" />
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Home;
