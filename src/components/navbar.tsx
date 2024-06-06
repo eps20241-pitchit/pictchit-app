d8acf94df9e1f18b1e8593ada511b46f1fd9d21c
Pedro Nogueira <88006041+phnog@users.noreply.github.com>
2024-06-06 15:56:47 -0300

    Add files via upload

---
+++ src/components/navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="src/assets/logo.png" alt="Logo" />
+      </div>
+      <ul className="navbar-links">
+        <li><a href="/" className="active">Home</a></li>
+        <li><a href="/quem-somos">Quem somos</a></li>
+        <li><a href="/contato">Contato</a></li>
+      </ul>
+      <div className="navbar-buttons">
+        <button className="login-button">Login</button>
+        <button className="register-button">Cadastro</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
