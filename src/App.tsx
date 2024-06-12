8be2ec7fb3f725132d21e5c99fd00ff89880177f
mibasFerraz <lucasllff@gmail.com>
2024-06-11 21:47:34 -0300

    tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Introduction from './pages/Intro/intro';
 import Register from './pages/Register/register';
 import Pitch from './pages/Pitch/pitch';
 import { CreatePitch } from './pages/CreatePitch';
-
+import Login from './pages/Login/login';
 
 export function App() {
   return (
@@ -16,8 +16,9 @@ export function App() {
           <Route path="/"  element={<Home/>} />
           <Route path="/introduction" element={<Introduction/>} />
           <Route path="/register" element={<Register/>} />
-          <Route path="/pitch" element={<Pitch/>}/>
-          <Route path="/create-pitch" element={<CreatePitch/>}/>
+          <Route path="/pitch" element={<Pitch/>} />
+          <Route path="/create-pitch" element={<CreatePitch/>} />
+          <Route path="/login" element={<Login/>} />
         </Routes>
       </div>
     </Router>
