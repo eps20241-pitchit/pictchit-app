bf2e8b0255b122cb1df20208364480e8684736f1
Iago <iagocabral.software@gmail.com>
2024-06-21 18:55:29 -0300

    login e registro de usuario
    Co-authored-by: Iago Cabral <iagocabral.software@gmail.com>
    Co-authored-by: Pedro Nogueira <190094486@aluno.unb.br>

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,6 +1,34 @@
 import imagemLogin from '../../assets/imagem-login.svg'
+import React, { useState} from 'react';
+import { getUser, saveLoggedInUser } from '../../service/login.service';
+import { useNavigate } from 'react-router-dom'; // Import the Redirect component
 
 const Login = () => {
+  const [usuario, setUsuario] = React.useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado para controlar se o usuário está logado
+  const [loginError, setLoginError] = useState(false);
+  const navigate = useNavigate(); // Correctly get the navigate function
+
+  const saveUser = (e: React.FormEvent) => {
+    e.preventDefault(); // Previne o reload da página
+    console.log(usuario);
+    getUser(usuario).then((response) => {
+      console.log(response);
+      if (response) {
+        setIsLoggedIn(true); // Atualiza o estado para refletir que o usuário está logado
+        saveLoggedInUser(response); 
+        navigate('/pitch');
+      } else {
+        setLoginError(true); // Exibe mensagem de erro se não encontrar o usuário
+      }
+    });
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-md rounded-lg p-8 flex flex-col md:flex-row md:max-w-4xl">
@@ -13,22 +41,24 @@ const Login = () => {
         </div>
         <div className="md:w-1/2 md:pl-6">
           <h2 className="text-2xl font-bold mb-6">Login</h2>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={saveUser}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                <input
+                onChange={(e) => setUsuario({ ...usuario, email: e.target.value })}
                 type="text"
                 placeholder="E-mail"
                 className="input-field"
               />
               <input
-                type="text"
+                onChange={(e) => setUsuario({ ...usuario, password: e.target.value })}
+                type="password"
                 placeholder="Senha"
                 className="input-field"
               />
             </div>
             <div className="flex justify-end">
-              <button type="button" className="btn btn-cancel">
-                Login
+              <button type="submit" className="btn btn-cancel">
+                Login1
               </button>
             </div>
           </form>
--- src/service/login.service.tsx
+++ src/service/login.service.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const API = axios.create({
-    timeout: 5000
+    timeout: 10000 // Aumente o tempo limite para 10 segundos
 });
 
 // Defina a interface para o objeto de usuário
@@ -17,17 +18,69 @@ interface CreateUserResponse {
     usuario: User;
 }
 
+interface GetUsersResponse {
+    usuarios: User[];
+}
+
 export async function createUser(objeto: User): Promise<CreateUserResponse | undefined> {
     try {
         console.log(objeto);
         const { data } = await API.post<CreateUserResponse>(
-            "http://localhost:3001/users",
+            "https://ms-users-y0ix.onrender.com/users",
             objeto
         );
         return data;
-    } catch (error) {
-        console.error(error);
+    } catch (error: any) {
+        if (axios.isAxiosError(error)) {
+            console.error('Axios error:', error.message);
+            if (error.response) {
+                console.error('Response data:', error.response.data);
+                console.error('Response status:', error.response.status);
+            }
+        } else {
+            console.error('Unexpected error:', error);
+        }
     }
 }
 
-    
+export async function getUser(objeto: User): Promise<User | undefined> {
+    try {
+        console.log(objeto);
+        const { data } = await API.get<User[]>(
+            "https://ms-users-y0ix.onrender.com/users"
+        );
+
+        // Verificar se o usuário com o email e senha fornecidos existe no array de usuários
+        const usuarioEncontrado = data.find(user =>
+            user.email === objeto.email && user.password === objeto.password
+        );
+
+        return usuarioEncontrado;
+    }
+         catch (error: any) {
+        if (axios.isAxiosError(error)) {
+            console.error('Axios error:', error.message);
+            if (error.response) {
+                console.error('Response data:', error.response.data);
+                console.error('Response status:', error.response.status);
+            }
+        } else {
+            console.error('Unexpected error:', error);
+        }
+    }
+}
+
+export function saveLoggedInUser(user: User) {
+    localStorage.setItem('loggedInUser', JSON.stringify(user));
+}
+
+export function getLoggedInUser(): User | null {
+    const userString = localStorage.getItem('loggedInUser');
+    return userString ? JSON.parse(userString) : null;
+}
+
+export function logout() {
+    localStorage.removeItem('loggedInUser');
+}
+
+
