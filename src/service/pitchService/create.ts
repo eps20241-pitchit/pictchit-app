1f4f179576549d5df217c220107e4f68cfcbf5b5
Pedro Nogueira <88006041+phnog@users.noreply.github.com>
2024-06-25 23:37:29 -0300

    Add files via upload

---
+++ src/service/pitchService/create.ts
@@ -0,0 +1,11 @@
+import { httpClient } from "../httpClient";
+
+export interface CompletionParams {
+  question: string;
+}
+
+export async function Create(params: CompletionParams) {
+  const { data } = await httpClient.post('https://pitchit-api.onrender.com/pitch/create', params);
+
+	return data;
+}
