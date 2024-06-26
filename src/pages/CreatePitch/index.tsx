372af375b860a077f5fb727a769e55924ba1f8f2
Douglas Castro <farias.douglascastro@gmail.com>
2024-06-26 03:02:26 -0300

    Atualização dos estilos

--- src/pages/CreatePitch/index.tsx
+++ src/pages/CreatePitch/index.tsx
@@ -11,7 +11,9 @@ export function CreatePitch() {
 
       <div className="flex justify-center items-center w-full h-screen">
         <div className="rounded-lg h-[485px] w-[571px] bg-white p-8 shadow-xl">
-          <header className="font-normal text-5xl text-[#003366] mb-8">Gerar Pitch</header>
+          <header className="font-normal text-5xl text-[#003366] mb-8">
+            <h2 className="text-2xl font-bold mb-6 text-[#003366]">Gerar Pitch</h2>
+          </header>
           <div className="flex flex-col justify-center items-start">
             <form onSubmit={handleSubmit}>
               <label htmlFor="nome" className="text-base text-[#003366]">Nome</label>
