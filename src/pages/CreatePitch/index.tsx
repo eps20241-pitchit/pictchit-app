d36b32532f57bcb0c5bfb9ca437e7c72d6f6b891
Pedro Nogueira <88006041+phnog@users.noreply.github.com>
2024-07-07 19:29:15 -0300

    Update index.tsx

--- src/pages/CreatePitch/index.tsx
+++ src/pages/CreatePitch/index.tsx
@@ -1,5 +1,6 @@
 import { Spinner } from "../../components/Spinnet";
 import { useCreatePitchController } from "./useCreatePitchController"
+import Chatbot from "../../components/chatbot";
 
 export function CreatePitch() {
   const {register, handleSubmit, errors, isLoading} = useCreatePitchController();
@@ -9,7 +10,7 @@ export function CreatePitch() {
       <div className="w-full h-full relative">
         <div className="bg-[#003366] w-[1000px] h-[1000px] -z-10 fixed top-0 -right-1/4 rounded-full "></div>
       </div>
-
+      <Chatbot />
       <div className="flex justify-center items-center w-full h-screen">
         <div className="rounded-lg h-[485px] w-[571px] bg-white p-8 shadow-xl">
           <header className="font-normal text-5xl text-[#003366] mb-8">
