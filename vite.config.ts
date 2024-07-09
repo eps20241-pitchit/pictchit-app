e601f7b22d8360e68f66378639227336c3c213d3
Pedro Nogueira <88006041+phnog@users.noreply.github.com>
2024-07-09 10:53:05 -0300

    fix: backup

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
-    port: 3003,
-    proxy: {
-      '/api': {
-        target: 'https://5a7udyuiimjx3rngjs7lp4dxee0phmbl.lambda-url.us-east-1.on.aws',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+    port: 3003
   }
 })
